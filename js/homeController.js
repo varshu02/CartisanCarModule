9abe13ebee26a071db0951a24937627c28a3437d
varshu02 <varshu250693@gmail.com>
2018-02-26 00:07:24 +0530

    Car Module

---
+++ js/homeController.js
@@ -0,0 +1,55 @@
+﻿var app = angular.module('MyApp', [])
+app.controller('homeController',
+    function($scope, $window) {
+
+        $scope.navigateNext = function () {
+            window.location.href = 'option.html';
+
+        }
+
+    });
+
+    app.controller('optionController',
+    function ($scope, $window) {
+
+        $scope.carRoleForm = false;
+        $scope.workShopRoleForm = false;
+        $scope.submitted = false;
+        $scope.errorMsg = false;
+        $scope.result = {
+            name: "",
+            phone: "",
+            email: "",
+            make: "",
+            model: "",
+            repairs: "",
+            files: "" 
+        }
+
+        $scope.phoneNumbr = /^\+?\d{10}$/;
+        
+
+
+        $scope.carRole = function () {
+            $scope.carRoleForm = true;
+            $scope.workShopRoleForm = false;
+
+        }
+
+        $scope.workShopRole = function () {
+            $scope.workShopRoleForm = true;
+            $scope.carRoleForm = false;
+
+        }
+
+        $scope.Submit = function (result) {
+            var details = [];
+            details.push(result);
+            if ($scope.result.name == "" || $scope.result.phone == "" || $scope.result.email == "" || $scope.result.make == "" || $scope.result.model == "" || $scope.result.repairs == "") {
+                $scope.errorMsg = true;
+            }
+            else {
+                $scope.submitted = true;
+            }
+        }
+    });
